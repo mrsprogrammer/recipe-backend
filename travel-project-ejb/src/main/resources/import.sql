7f3af08a5f9a74a1d9c6772cb778db4dc20406ab
justyna <you@example.com>
2020-10-03 21:15:33 +0200

    mysql connection to travel database working :)

--- travel-project-ejb/src/main/resources/import.sql
+++ travel-project-ejb/src/main/resources/import.sql
@@ -1,2 +1,10 @@
 -- You can use this file to load seed data into the database using SQL statements
-insert into Member (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212') 
+insert into Member (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212');
+
+-- CREATE TABLE Member (
+-- id INT NOT NULL AUTO_INCREMENT,
+-- name varchar(255) NOT NULL,
+-- email varchar(255) NOT NULL,
+-- phone_number varchar(255),
+-- PRIMARY KEY (id)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
